ca94a3e6b26f428232a91e28d6d86f3a958e35d9
KINN05 <rifqialkindi57@gmail.com>
2025-10-17 11:50:53 +0700

    Update form-looping.php

--- form-looping.php
+++ form-looping.php
@@ -1,5 +1,39 @@
-<?php
-    for($i=10; $i >= 1; $i--){
-        echo "Perulangan ke-$i <br>";
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        Tanggal:
+        <select>
+        <?php
+            for($day=1; $day <= 31; $day++){
+                echo "<option>$day</option>";
+            }
+        ?> 
+        </select>
+        
+        Bulan:
+        <select>
+        <?php
+            $bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+            foreach($bulan as $b){
+                echo "<option>$b</option>";
+                
+            }
+        ?>
+        </select>
+
+        Tahun:
+        <select>
+        <?php
+            for($tahun=2025; $tahun >= 1970; $tahun--){
+                echo "<option>$tahun</option>";
+            }
+        ?>
+    </form>
+</body>
+</html>
