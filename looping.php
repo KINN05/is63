bc9446c44c353800ab2ec80a8b296bda155e8257
KINN05 <rifqialkindi57@gmail.com>
2025-10-17 09:53:51 +0700

    Update looping.php

--- looping.php
+++ looping.php
@@ -3,4 +3,37 @@
 for($i=1; $i <= 10; $i++){
     echo "Perulangan ke-$i <br>";
 }
+echo "<hr>";
+for($i=10; $i >= 1; $i--){
+        echo "Yang ke-$i <br>";
+}
+
+echo "<hr>";
+### WHILE ###
+$awal = 1;
+while($awal <= 10){
+    echo "Rumah ke-$awal <br>";
+    $awal++;
+}
+
+echo "<hr>";
+### DO WHILE ###
+$awal2 = 1;
+do{
+    echo "Mahasiswa ke-$awal2 <br>";
+    $awal2++;
+}while($awal2 <= 10);
+
+// echo "<hr>";
+// ### FOREACH ###
+// $warna = array("Merah", "Kuning", "Hijau", "Biru", "Ungu");
+// foreach($warna as $w){
+//     echo "Warna $w <br>";
+// }
+
+// echo "<hr>";
+// $angka = array(10, 20, 30, 40, 50);
+// foreach($angka as $a){
+//     echo "Angka $a <br>";
+// }
 ?>
