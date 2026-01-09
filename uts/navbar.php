ba6bd7bea2873f0f0581cdc8b825813579035181
KINN05 <rifqialkindi57@gmail.com>
2026-01-09 09:48:59 +0700

    Create navbar.php

---
+++ uts/navbar.php
@@ -0,0 +1,19 @@
+
+<nav class="navbar navbar-expand-lg" style="background-color: #FAF9F3;" data-bs-theme="white">
+  <div class="container-fluid">
+    <a class="navbar-brand mb-0 h1" href="http://localhost/IS63/uts/">HOME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="http://localhost/is63/uts/rifqi/">MY PRODUK</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
