f121e500794570fc2077d9215d10a1058e4f752c
KINN05 <rifqialkindi57@gmail.com>
2026-01-31 17:28:22 +0700

    Create proses_edit.php

---
+++ biodataBelajar/gelombang/proses_edit.php
@@ -0,0 +1,22 @@
+<?php
+#1. Meng-koneksikan PHP ke MySQL
+include("../koneksi.php");
+
+#2. Mengambil Value dari Form Tambah
+$id = $_POST["id"];
+$nm_gel = $_POST["nm_gel"];
+$tgl_awal = $_POST["tgl_awal"];
+$tgl_akhir = $_POST["tgl_akhir"];
+
+#3. Query Insert (proses tambah data)
+$query = "UPDATE gelombang SET nama_gelombang='$nm_gel', tgl_awal='$tgl_awal', tgl_akhir='$tgl_akhir'
+WHERE id='$id'";
+
+$tambah = mysqli_query($koneksi, $query);
+
+#4. Jika Berhasil triggernya apa? (optional)
+if ($tambah) {
+    header("location:index.php");
+} else {
+    echo "Data Gagal ditambah";
+}
