0cbd2e16c5aaa875c7c852f2c77b87ee58d8508f
KINN05 <rifqialkindi57@gmail.com>
2026-02-06 11:08:54 +0700

    login

--- biodataBelajar/navbar.php
+++ biodataBelajar/navbar.php
@@ -20,6 +20,9 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/gelombang/">Gelombang</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/logout.php">Logout</a>
+                </li>
 
             </ul>
             <form class="d-flex" role="search">
