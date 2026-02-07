d1f5f190ba8a1292eeee692a45a94c51ab8b67f0
KINN05 <rifqialkindi57@gmail.com>
2026-02-07 08:48:54 +0700

    .

--- uts/navbar.php
+++ uts/navbar.php
@@ -1,19 +1,34 @@
+<nav class="navbar navbar-expand-lg" style="background-color:#005e10" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="http://localhost/IS63/biodataBelajar/">DATA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/siswa/">Siswa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/jurusan/">Jurusan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/dosen/">Dosen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/gelombang/">Gelombang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/IS63/biodataBelajar/logout.php">Logout</a>
+                </li>
 
-<nav class="navbar navbar-expand-lg" style="background-color: #FAF9F3;" data-bs-theme="white">
-  <div class="container-fluid">
-    <a class="navbar-brand mb-0 h1" href="http://localhost/IS63/uts/">HOME</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-      aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="http://localhost/is63/uts/rifqi/">MY PRODUK</a>
-        </li>
-      </ul>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
     </div>
-  </div>
 </nav>
